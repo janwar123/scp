786532e0fcd08e0e80331ef9cd6d11dd593060c7
janwar123 <janwarjames123@gmail.com>
2026-08-16 00:16:51 +0600

    Enhance UDP client with error handling and closure
    Added error handling for keyboard interrupt and client closure.

--- socket final/Exercises 6.5/UDP Client.py
+++ socket final/Exercises 6.5/UDP Client.py
@@ -1,16 +1,24 @@
 import socket
 
-HOST="127.0.0.1"
-PORT=5000
+HOST = "127.0.0.1"
+PORT = 5000
 
-client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
 
-    msg=input("You : ")
+    try:
 
-    client.sendto(msg.encode(),(HOST,PORT))
+        message = input("Client: ")
 
-    reply,address=client.recvfrom(1000)
+        client.sendto(message.encode(), (HOST, PORT))
 
-    print("Server :",reply.decode())
+        reply, addr = client.recvfrom(1000)
+
+        print("Server:", reply.decode())
+
+    except KeyboardInterrupt:
+        print("\nClient Closed")
+        break
+
+client.close()
