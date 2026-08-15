c48d0f485f7dee573af0459608a1fe78dcd68853
janwar123 <janwarjames123@gmail.com>
2026-08-16 00:02:12 +0600

    new bond

--- socket final/Exercises_6.7/multicast_chat.py
+++ socket final/Exercises_6.7/multicast_chat.py
@@ -1,78 +1,50 @@
 import socket
+#struct use to format binary data
 import struct
 import threading
 
-
-MULTICAST_GROUP = "224.1.1.1"
+GROUP = "224.1.1.1"
 PORT = 5000
 
-
-
-def receive_messages(sock):
-
-    while True:
-
-        data, address = sock.recvfrom(1024)
-
-        print(f"\n{address}: {data.decode()}")
-        print("You: ", end="", flush=True)
-
-
-def send_messages(sock):
-
-    while True:
-
-        message = input("You: ")
-
-        sock.sendto(
-            message.encode(),
-            (MULTICAST_GROUP, PORT)
-        )
-
-
-sock = socket.socket(
-    socket.AF_INET,
-    socket.SOCK_DGRAM,
-)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 sock.bind(("", PORT))
 
 
-group = socket.inet_aton(MULTICAST_GROUP)
+group = socket.inet_aton(GROUP)
 
-membership = struct.pack(
-    "4sL",
-    group,
-    socket.INADDR_ANY
-)
+mreq = struct.pack("4sL", group, socket.INADDR_ANY)
 
 sock.setsockopt(
     socket.IPPROTO_IP,
     socket.IP_ADD_MEMBERSHIP,
-    membership
+    mreq
 )
 
-print("Joined multicast group.")
-print("Start chatting...\n")
+
+def receive():
+
+    while True:
+
+        data, address = sock.recvfrom(1024)
+
+        print("\nReceived:", data.decode())
 
 
-receive_thread = threading.Thread(
-    target=receive_messages,
-    args=(sock,),
-    daemon=True
-)
+thread = threading.Thread(target=receive)
 
-receive_thread.start()
+thread.daemon = True
+
+thread.start()
 
 
-try:
+while True:
 
-    send_messages(sock)
+    message = input("You: ")
 
-except KeyboardInterrupt:
-
-    print("\nChat closed.")
-
-    sock.close()
+    sock.sendto(
+        message.encode(),
+        (GROUP, PORT)
+    )
